38ad2541dacc9dd8a9ff1130ef90812719bd30cb
francivaldolima <francivaldolimadesousa@gmail.com>
2021-11-29 09:29:49 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        int a=5;
+        int b=3;
+        System.out.println("Hello World " + ( a + b));
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+    }
+}
